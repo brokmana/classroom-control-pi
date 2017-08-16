44b884f266f5a9e5f71e26d9b8dae9e363299dba
brokmana <brokmana@gmail.com>
2017-08-16 16:35:54 -0400

    bas.rc file

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -8,5 +8,12 @@ class skeleton {
   
   # add a resource to manage /etc/skel/.bashrc
 
+file { '/etc/skel/.bashrc':
+ensure => file,
+owner => 'root',
+group => 'root',
+mode => '0644',
+source => 'puppet:///modules/skeleton/bashrc',
+}
 
 }
