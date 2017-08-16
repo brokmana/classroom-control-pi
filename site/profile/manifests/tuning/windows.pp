c8b66b6ba14349038f2d1099feecaed9dbcee387
Oluwayomi Joseph <yomlinux@gmail.com>
2017-08-16 17:01:47 -0400

    Update windows.pp

--- site/profile/manifests/tuning/windows.pp
+++ site/profile/manifests/tuning/windows.pp
@@ -5,6 +5,15 @@ class profile::tuning::windows {
       data   => '1',
     }
 
-  # Add more registry resources as directed
+  # Add more registry resources as directed    
   
+     registry::value { 'KeepAliveInterval':
+     key    => 'HKLM\SYSTEM\CurrentControlSet\Services\AFD\Parameters',
+     value  => 'TcpMaxDataRetransmissions',
+     data   => '5',
+  
+     registry::value { 'KeepAliveInterval':
+     key    => 'HKLM\SYSTEM\CurrentControlSet\Services\AFD\Parameters',
+     value  => 'TcpTimedWaitDelay',
+     data   => '0x0000001e',
 }
