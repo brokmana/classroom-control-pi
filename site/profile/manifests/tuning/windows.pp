d49ff031e2b94f182b343b48da91761ca84034b9
michelebeverly <30533116+michelebeverly@users.noreply.github.com>
2017-08-16 14:59:14 -0600

    lab 12.1 manage windows registry keys

--- site/profile/manifests/tuning/windows.pp
+++ site/profile/manifests/tuning/windows.pp
@@ -7,4 +7,18 @@ class profile::tuning::windows {
 
   # Add more registry resources as directed
   
+  registry::value { 'TcpMaxDataRetransmissions':
+      key    => 'HKLM\SYSTEM\CurrentControlSet\Services\TCPIP\Parameter',
+      value  => 'TcpMaxDataRetransmissions',
+      data   => '5',
+      
+      registry::value { 'cpTimedWaitDelay':
+      key    => 'HKLM\SYSTEM\CurrentControlSet\Services\TCPIP\Parameters',
+      value  => 'cpTimedWaitDelay',
+      data   => '0x0000001e',
+  
+  
+  
+  
+  
 }
