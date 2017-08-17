a106ee5adafcc46cae269de648a83070c501a584
brokmana <brokmana@gmail.com>
2017-08-17 14:12:39 -0400

    16.1

--- manifests/site.pp
+++ manifests/site.pp
@@ -23,6 +23,8 @@
 
 node default {
   # This is where you can declare classes for all nodes.
+  notify { "The primary disk is ${::disks['sda']['size']} in size.": }
+
   # Example:
   #   class { 'my_class': }
   
